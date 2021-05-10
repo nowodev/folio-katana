2f855d5946446549dc231cb0669c0c875dadbae2
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-10 02:11:23 +0100

    include nav & footer

--- content/_includes/base.blade.php
+++ content/_includes/base.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="@yield('pageDescription', $siteDescription)">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="@yield('pageDescription', $siteDescription)">
 
-    <title>{{$siteName}} @yield('pageTitle')</title>
-
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
-    {{-- <link rel="stylesheet" href="@url('assets/css/all.css')"> --}}
+  <title>{{ $siteName }} @yield('pageTitle')</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css">
 </head>
 
 <body>
+  @yield('body')
 
-<div class="wrapper m-t-30">
-    @yield('body')
-</div>
+  @include('_includes.footer')
+
+  <!-- AlpineJS Library -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.8.0/alpine.js"></script>
 
 </body>
+
 </html>
